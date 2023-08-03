ec71ad7d218125d2d5810fb9f72464aa4f21bea1
dave.chia <dave.chia@rapidz.io>
2023-08-03 19:33:33 +0800

    Removed redundant default users api route

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('verify', [\App\Http\Controllers\VerificationController::class, 'verify'])->name('verify');
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
