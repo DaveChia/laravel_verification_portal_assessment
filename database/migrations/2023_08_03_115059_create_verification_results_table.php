3c2a1e5df7c9d055bdbbcced9195ace339d5cd3e
dave.chia <dave.chia@rapidz.io>
2023-08-03 19:53:35 +0800

    Created a laravel database migration to create verification_results table

---
+++ database/migrations/2023_08_03_115059_create_verification_results_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVerificationResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verification_results', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_type', 20)->index();
+            $table->string('verification_result', 20)->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('verification_results');
+    }
+}
