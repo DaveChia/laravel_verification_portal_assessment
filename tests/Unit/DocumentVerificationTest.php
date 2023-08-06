aa3c0846946bf91ce2c10866bfb3d090b139dda1
dave.chia <dave.chia@rapidz.io>
2023-08-06 10:39:56 +0800

    Added Unit Test to test the creation of a new JsonDocument class

--- tests/Unit/DocumentVerificationTest.php
+++ tests/Unit/DocumentVerificationTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\DataTransferObjects\JsonDocument;
+use Illuminate\Http\UploadedFile;
 use PHPUnit\Framework\TestCase;
 
 class DocumentVerificationTest extends TestCase
@@ -17,8 +19,35 @@ class DocumentVerificationTest extends TestCase
         $this->assertEquals($target_hash, $hashed_data);
     }
 
-    public function test_example()
+    public function testJsonDocumentCreation()
     {
+        $json_file_to_test = json_decode(
+            '{
+                "data": {
+                  "id": "63c79bd9303530645d1cca00",
+                  "name": "Certificate of Completion",
+                  "recipient": {
+                    "name": "Marty McFly",
+                    "email": "marty.mcfly@gmail.com"
+                  },
+                  "issuer": {
+                    "name": "Accredify",
+                    "identityProof": {
+                      "type": "DNS-DID",
+                      "key": "did:ethr:0x05b642ff12a4ae545357d82ba4f786f3aed84214#controller",
+                      "location": "ropstore.accredify.io"
+                    }
+                  },
+                  "issued": "2022-12-23T00:00:00+08:00"
+                },
+                "signature": {
+                  "type": "SHA3MerkleProof",
+                  "targetHash": "288f94aadadf486cfdad84b9f4305f7d51eac62db18376d48180cc1dd2047a0e"
+                }
+              }'
+        );
+
+        new JsonDocument($json_file_to_test);
 
         $this->assertTrue(true);
     }
