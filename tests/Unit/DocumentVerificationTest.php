970c4ec3e743696047cd0f6b1ff979d81e028c5b
dave.chia <dave.chia@rapidz.io>
2023-08-06 10:50:17 +0800

    Added unit test to JsonDocument's extended verifyDocumentHasValidRecipient method

--- tests/Unit/DocumentVerificationTest.php
+++ tests/Unit/DocumentVerificationTest.php
@@ -51,4 +51,37 @@ class DocumentVerificationTest extends TestCase
 
         $this->assertTrue(true);
     }
+
+    public function testJsonDocumentHasValidRecipient()
+    {
+        $json_file_to_test = json_decode(
+            '{
+                "data": {
+                  "id": "63c79bd9303530645d1cca00",
+                  "name": "Certificate of Completion",
+                  "recipient": {
+                    "name": "Marty McFly",
+                    "email": "marty.mcfly@gmail.com"
+                  },
+                  "issuer": {
+                    "name": "Accredify",
+                    "identityProof": {
+                      "type": "DNS-DID",
+                      "key": "did:ethr:0x05b642ff12a4ae545357d82ba4f786f3aed84214#controller",
+                      "location": "ropstore.accredify.io"
+                    }
+                  },
+                  "issued": "2022-12-23T00:00:00+08:00"
+                },
+                "signature": {
+                  "type": "SHA3MerkleProof",
+                  "targetHash": "288f94aadadf486cfdad84b9f4305f7d51eac62db18376d48180cc1dd2047a0e"
+                }
+              }'
+        );
+
+        $jsonDocument = new JsonDocument($json_file_to_test);
+
+        $this->assertTrue($jsonDocument->verifyDocumentHasValidRecipient());
+    }
 }
